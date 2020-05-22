0f604e23c21d2559acda871b21a5ab414b6e6b38
Dawid <dwilk92@o2.pl>
2020-05-22 18:35:26 +0200

    zad petle- gotowe

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+//for(let i = 1; i<=x; i++)
+//{
+  //let o= result += i
+  //console.log(o)
+//}
+let i = 0;
+while(i< x)
+{
+    i++;
+   console.log(result+=i);
+   
+}
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,18 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(iteration <= x)
+{
+    randomNumber = Math.round(Math.random() * 15);
+    if(randomNumber === x)
+    {
+        console.log(iteration)
+    }
+    //else{
+       // console.log('o')
+   // }
+    //console.log(randomNumber)
+    iteration++;
+}
+
