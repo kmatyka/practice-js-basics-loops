5300f1c35c7095ff8df1d81b62efc9e95092b29a
Martyna Kiljan <kmartynak@icloud.com>
2020-05-27 11:52:01 +0200

    petla1

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,12 @@
-const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+let randomNumber = Math.round(Math.random() * 20);
+//  console.log(randomNumber);
+
+
+for (let i = 5; i < randomNumber; i++) {
+    if (5 < randomNumber) {
+        console.log(i);
+    } else {
+        console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+    }
+
+}
