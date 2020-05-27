d9a64468a2c8a41b61bd45ebb389f0a451e1e681
Martyna Kiljan <kmartynak@icloud.com>
2020-05-27 11:52:49 +0200

    petla2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,21 @@
 const x = 5;
 let result = 0;
+
+// for (i = 1; i < x + 1; i++) {
+
+//     // result += i;
+//     result = result + i;
+//     console.log(result)
+// }
+
+
+const x = 5;
+let result = 0;
+let i = 1;
+
+while (i < 6) {
+
+    result += i;
+    console.log(result);
+    i++;
+}
