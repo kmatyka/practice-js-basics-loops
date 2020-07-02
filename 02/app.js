8539eb6ff6edc4541e4586f01dd13a27275770af
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-02 17:45:42 +0200

    changes made

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,17 @@
 const x = 5;
 let result = 0;
+let result1 = 0;
+let j = 0;
 
 for (let i = 1; i <= 5; i++) {
     result = result + i;
     console.log(result);
+}
+
+
+
+while (j < x) {
+    j++
+    result1 = result1 + j
+    console.log(result1)
 }
