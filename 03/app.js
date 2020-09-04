b14aaa3b12b244001d7f9052451987be39aa8d83
AnnaBuller <aniabela@wp.pl>
2020-09-04 15:26:12 +0200

    solve 03 task

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,14 @@
-const x = 10;
-let iteration = 0;
-let randomNumber = -1;
+// Zdecydowałam, by ograniczyć przedział losowanych liczb, czyniąc x największą liczbą w przedziale.
+const x = 3;
+let iteration = 1;
+let randomNumber = 0;
+
+while (randomNumber !== x) {
+	randomNumber = Math.round(Math.random() * x);
+	if (randomNumber === x) {
+		console.log('Good! You hit ' + x + ' after' + ' ' + iteration + ' times.');
+	} else {
+		console.log(randomNumber + ' is not what we want and we want ' + x);
+	}
+	iteration++;
+}
