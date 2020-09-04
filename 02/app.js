057a122f9bb791e3695461e8363946677a87f9b8
AnnaBuller <aniabela@wp.pl>
2020-09-04 13:41:57 +0200

    solve 02 task

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
+// console.log('result');
+
 const x = 5;
 let result = 0;
+
+// for (i = 0; i <= x; i++) {
+// 	result += i;
+// }
+
+let j = 0;
+while (j <= 5) {
+	result += j;
+	j++;
+}
+
+console.log(result);
