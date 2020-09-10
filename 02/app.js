12ed3d6aefbcc7cb3f05c23ac4469c2b31d23a0f
Piotr Niemyjski <piotr.niemyjski@wp.pl>
2020-09-10 15:32:10 +0200

    first version

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,18 @@
 const x = 5;
 let result = 0;
+let i = 1;
+
+//petla for
+for(i = 1; i <= x; i++) {
+    result += i;
+}
+console.log('Wynik z wykorzystaniem pętli for: ' + result);
+
+//petla while
+result = 0;
+i = 1;
+while(i <= x) {
+    result += i;
+    i++;
+}
+console.log('Wynik z wykorzystaniem pętli while: ' + result);
