bf892cdd8b1cbc978da5662bc815f1ad58798745
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-09 17:04:57 +0200

    Add for and while loop in Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+//Rozwiązanie korzystające z pętli for
+// for (let i = 0; i <= x; i++) {
+//     result = result + i;
+// }
+
+
+// Rozwiązanie korzystające z pętli while
+let i = 0;
+while (i < 5) {
+    i++;
+    result = result + i;
+    console.log(result);
+}
