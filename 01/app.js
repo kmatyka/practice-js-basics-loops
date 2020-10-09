84c8a22f4e0df71e191dcdb4998dc47571519b40
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-09 16:58:15 +0200

    Add if-else and for loop in Task 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+
+if (randomNumber > 5) {
+    for (let i = randomNumber; i >= 0; i--) {
+        console.log(i);
+    }
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
