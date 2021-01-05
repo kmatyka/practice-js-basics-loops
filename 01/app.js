0dfdece9661378a39d9b7f26a5f9c4f83f5b9fa0
Karolina Jedrajczyk <kalka@MacBook-Pro-Karolina.local>
2021-01-05 09:21:52 +0100

    message

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+
+
+if(randomNumber > 5) {
+    for(let i=randomNumber; i<=20; i++) {
+            console.log(i);}
+    } else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
+
