b6a2143b2bfb604eb660d4a365d9781b849289eb
Adam Jochemczyk <adamjoche@gmail.com>
2021-02-10 11:07:29 +0100

    Add summation by for and while loop

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+for(let i=1;i<=x;i++){
+    result+=i;
+}
+console.log("For:",result)
+result=0;
+let iterator=0;
+while(iterator<=x){
+    result+=iterator;
+    iterator++;
+}
+console.log("While:", result);
