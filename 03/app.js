4885b9bfd2e051d60dfd2a0f0ff67d8c5108e192
Adam Jochemczyk <adamjoche@gmail.com>
2021-02-10 11:17:23 +0100

    Add getRandomNumber and count iterations.

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
-const x = 10;
+const x = 10;//10
 let iteration = 0;
 let randomNumber = -1;
+
+const getRandomNumber =()=>{
+    return Math.round(Math.random() * 10);
+}
+
+while(randomNumber!==x){
+    randomNumber=getRandomNumber();
+    iteration++;
+}
+console.log(`Liczba ${x} wylosowana za ${iteration} razem`)
