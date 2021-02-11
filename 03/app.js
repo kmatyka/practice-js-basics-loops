e3f7fbccfd41879d0b965e71a7375ce0a3a647f0
Agnieszka Polińska <agapoli@gmail.com>
2021-02-11 14:28:52 +0100

    Use while to draw x and log needed iterations

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+    iteration++;
+    randomNumber = Math.floor(Math.random() * 20+1);
+}
+
+console.log(iteration);
+
+
