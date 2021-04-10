d91d519d41fafd74208a57eeebb636e4a079c70c
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-04-11 00:32:16 +0200

    I ended task 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while(randomNumber !== x) {
+
+    randomNumber = Math.round(Math.random() * 15);
+    iteration = iteration + 1;
+    console.log(randomNumber);
+}
+
+alert('Brawo, udało Ci się za ' + iteration + 'razem');
