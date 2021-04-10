bcb9d634c986afb3b52988093d470e7f8ccc2530
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-04-10 22:51:10 +0200

    I ended task 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,14 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log('Wylosowana liczba to:' , randomNumber);
+
+const limitNumber = 5;
+
+if (randomNumber > limitNumber) {
+    for(let i=limitNumber; i<=randomNumber; i++) {
+       console.log(i);
+    }
+}
+
+else {
+  console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
