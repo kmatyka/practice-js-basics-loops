965d42792c065748eb00fd8a990d9bf2d8de4437
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-18 10:09:57 +0200

    Taks 01
    Task 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,15 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log('Wylosowana liczba to ' + randomNumber);
+
+if (randomNumber > 5) {
+    for (let i = 5; i <= randomNumber; i++) {
+        if (i <= randomNumber) {
+            console.log(i);
+        }
+    }
+}
+
+if (randomNumber <= 5) {
+    console.log('Wylosowana liczba jest zbyt mała by użyć pętli!')
+}
+
