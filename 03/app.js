83e9144cf726e92f9346cd9f31f9a0fa94b12199
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-04-18 10:53:14 +0200

    Task 03
    Task 03: while loop

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+console.log('Sprawdźmy, za którym razem, losując liczby z przedziału 0 - 30, uda się wylosować liczbę 10')
+
+while (randomNumber !== x) {
+    iteration++;
+    randomNumber = Math.floor(Math.random() * 31);
+    console.log('Za ' + iteration + ' razem wylosowano ' + randomNumber)
+}
+
+console.log('Liczba losowań = ' + iteration)
