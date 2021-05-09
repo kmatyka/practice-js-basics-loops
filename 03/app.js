985b668966c0b653f1c71fa8409321d980484805
Krzysiek Swol <krzysiek.swol@gmail.com>
2021-05-09 07:56:33 +0200

    loops finished

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while (randomNumber !== x){
+  randomNumber = Math.round(Math.random() * x);
+  iteration++;  
+  console.log(`${iteration} numer iteracji - ${randomNumber} wylosowana liczba`);
+}
+console.log(`${iteration} - finalna iteracja`)
+
+
