791481df8b030beeed09670b6aed10a4ac482ddc
Krzysiek <krzysiek.swol@gmail.com>
2021-05-10 17:46:37 +0200

    loop while

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,19 @@
 const x = 5;
 let result = 0;
 
-for (i = result; i <= x; i++){
+for (i = 0; i <= x; i++){
   result += i
 
 }
 console.log(result);
+
+// ////////////////////////////////// while
+
+let resultTwo = 0;
+let j = 0;
+while(j <= x) {
+  resultTwo += j;
+  j++;
+}
+console.log(resultTwo);
+
