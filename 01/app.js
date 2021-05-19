60e784ac9ffef8778948d1c83f153685767c5c87
Andrzej <andrzej.rakowski79@gmail.com>
2021-05-19 15:52:45 +0200

    Zadania nr 1 i 2 zrobione

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,12 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if(randomNumber > 5) {
+    console.log('wieksza od 5')
+    const diff = randomNumber - 5;
+    for(i = 5; i <= randomNumber; i++) {
+        console.log(i);
+    }
+} else {
+    console.log('Wylosowana liczba nie jest większa od 5');
+}
