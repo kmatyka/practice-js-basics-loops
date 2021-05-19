86801d258cfb52afa2410ede9b79925b73fdb578
Andrzej <andrzej.rakowski79@gmail.com>
2021-05-19 16:02:21 +0200

    Zadanie nr 3 zrobione

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(x !== randomNumber) {
+    randomNumber = Math.round(Math.random() * 20);
+    console.log(randomNumber);
+    iteration++;
+}
+
+console.log(`Liczbę ${x} udało się wylosować w ${iteration} iteracji`);
