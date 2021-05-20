a5129d2e9cec03ab99ea894931aaf060562497ec
Malgorzata Kowacka <kowackag@gmail.com>
2021-05-20 10:33:23 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x !== randomNumber) {
+    randomNumber = Math.round(Math.random() * x);
+    console.log(randomNumber);
+    iteration++;
+}
+console.log(iteration);
