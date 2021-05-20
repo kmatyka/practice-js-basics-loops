d8bce01c34749f8124636bed1526805b6875ff6c
Malgorzata Kowacka <kowackag@gmail.com>
2021-05-20 10:05:08 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,19 @@
 const x = 5;
 let result = 0;
+let result2 = 0;  //dla while
+
+// for
+
+for (let i = 1; i <= x; i++) {
+    result += i;
+    console.log(result);
+}
+
+// while 
+
+let j = 1;
+while (j <= x) {
+    result2 += j;
+    console.log(result2);
+    j++;
+}
