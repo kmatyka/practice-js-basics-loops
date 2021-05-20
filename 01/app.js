53ebce2d41964a6789f56a31cc9377bd3ee35ea0
Malgorzata Kowacka <kowackag@gmail.com>
2021-05-20 09:46:18 +0200

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+document.write('Wylosowana liczba to: ' + randomNumber + '<br>');
+
+if (randomNumber > 5) {
+    document.write('Liczby od 5 do wylosowanej wartości to:');
+    for (i = 5; i <= randomNumber; i++) {
+        document.write(i + ',');
+    }
+} else {
+    document.write('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
