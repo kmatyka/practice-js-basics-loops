daf4c8e421149248461484f08db6364dc5a22f8b
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-05-25 09:19:36 +0200

    done task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+ 
+
+
+while(randomNumber !== x) {
+  randomNumber = Math.floor(Math.random() * 12);
+    console.log(randomNumber);
+  iteration++;
+}
+
+console.log('Liczba losowań: ' + iteration)
