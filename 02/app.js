c3a2904be922ccb7f13b957e2976ad18ba2409bc
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-06-15 15:54:24 +0100

    02 solved

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,18 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+  result += i;
+}
+
+console.log(`Koncowy wynik to ${result}`);
+
+let whileResult = 0;
+let i = 0;
+
+while (i <= x) {
+  whileResult += i;
+  i++;
+}
+
+console.log(`Koncowy wynik to ${whileResult}`);
