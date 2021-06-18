3df54db0b985cd91e7d749f57c15ca2fa5fce35d
Michal Krawczyk <krawczykmichal1@gamil.com>
2021-06-18 10:04:50 +0100

    done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber<10) {
+    randomNumber = Math.round(Math.random()*10);
+    console.log(randomNumber);
+    iteration++;
+}
+console.log('petla wykonala sie' + iteration);
