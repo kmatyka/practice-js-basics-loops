ece0006e0c16b2a8d3548b0a345602714b82dfeb
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-07-26 19:05:34 +0200

    closes task 2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,18 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+    result += i;
+}
+
+console.log(`Wynik: ${result} (pętla for)`);
+
+let resultWhile = 0;
+let counter = 0;
+
+while(counter <= x) {
+    resultWhile += counter;
+    counter++;
+}
+
+console.log(`Wynik: ${resultWhile} (pętla while)`);
