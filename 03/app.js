1c43166adbc93814ac8ca73d6b940224e3821043
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-07-26 19:16:06 +0200

    closes task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * x);
+    iteration++;
+}
+
+console.log(`Liczba x = ${x} została wylosowana za ${iteration} razem :)`);
