2f6135ea6962181f79eac251d5f843bc2a463b58
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-05 17:08:03 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,26 @@
+//for version
+
+console.log("For version");
+
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+  result = result + i;
+  console.log(result);
+}
+console.log(result);
+
+//while version
+
+let anotherResult = 0;
+const y = 5;
+
+console.log("While version");
+let j = 0;
+
+while (j <= y) {
+  anotherResult = anotherResult + j;
+  console.log(anotherResult);
+  j++;
+}
