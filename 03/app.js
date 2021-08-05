6f83b923ec2056822dcd1f2ef713562cac9437f3
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-05 17:41:02 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber != x) {
+  randomNumber = Math.round(Math.random() * 20);
+  iteration++;
+}
+
+console.log(`Liczbę ${x} udało się wylosować po ${iteration} próbach.`);
