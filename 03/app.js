0c1b476d8889515c173473d02054a1fc975a2bc6
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-24 15:13:14 +0200

    Final task

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
-const x = 10;
+const x = 3;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * x);
+    iteration++;
+    // console.log(randomNumber);
+}
+console.log(iteration);
