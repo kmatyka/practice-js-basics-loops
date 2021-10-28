ed8c5e4773cf7abac3d16312093a8d09913ecce0
Rafał Kazik <rafalkazik@MacBook-Air-Rafal.local>
2021-10-28 14:55:00 +0200

    js loops task

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,23 @@
 const x = 5;
 let result = 0;
+
+//
+// FOR LOOP
+//
+
+for (let i = 0; i <= x; i++) {
+  result = result + i;
+  console.log(result);
+}
+
+//
+// WHILE LOOP
+//
+
+// console.log(result);
+// let j = 1;
+// while (j <= x) {
+//   result = result + j;
+//   j++;
+//   console.log(result);
+// }
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
-const x = 10;
+const x = 5;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x !== randomNumber) {
+  randomNumber = Math.round(Math.random() * 10);
+  iteration++;
+}
+
+console.log(
+  "W celu wylosowania liczby 5, losowanie odbyło się " + iteration + " razy"
+);
