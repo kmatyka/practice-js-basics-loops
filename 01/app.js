57162fa4b8efb54233f8be31dd7b597bf7fb629b
Rafał Kazik <rafalkazik@MacBook-Air-Rafal.local>
2021-10-29 14:32:10 +0200

    loops taks  amendment

--- 01/app.js
+++ 01/app.js
@@ -1,7 +1,7 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log("Wylosowana liczba, to: " + randomNumber);
 
-if (randomNumber > 5 && randomNumber <= 20) {
+if (randomNumber > 5) {
   for (let i = 5; i <= randomNumber; i++) {
     console.log(i);
   }
