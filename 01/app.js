0ee8b1b271f8750daf1a9844a783b45142fb8973
Ewelina <ewelina@ekopacz.pl>
2021-11-05 20:19:28 +0100

    updade for loop

--- 01/app.js
+++ 01/app.js
@@ -1,10 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log('Wylosowana liczba: ' + randomNumber);
-let j=randomNumber;
 
-for(j=randomNumber; j>=5 && j<=20; j--) {
+if(randomNumber>=5) {
+    for(let j=randomNumber; j>=5 && j<=20; j--)
     console.log(j);
-}
-if(randomNumber < 5) {
-    console.log('Nie mozna wykonać pętli liczba: ' + j + ' mniejsza od 5');
+    
+}else {
+    console.log('Nie mozna wykonać pętli liczba mniejsza od 5');
 }
