975339dd0aa6e824439c069307bb94348f7831bb
Ewelina <ewelina@ekopacz.pl>
2021-11-05 12:16:35 +0100

    add while loop

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,18 @@
 const x = 5;
 let result = 0;
 
+// For loop 
 for(let i = 0; i <= x; i++){
     result = result + i;
 }
 console.log(result);
+
+//While loop
+let result2 = 0;
+
+let j=0;
+while(j<=x) {
+    result2+=j;
+    j++
+}
+console.log(result2);
