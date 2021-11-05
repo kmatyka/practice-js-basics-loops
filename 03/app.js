da650a1fcb53fa7a909b4d5f0d5afcb5dbde1fac
Ewelina <ewelina@ekopacz.pl>
2021-11-05 13:34:34 +0100

    add while loop and random

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
-const x = 10;
+
+const x = 12;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+}
+console.log("Wylosowano liczbe 12 za " + iteration + " razem" );
