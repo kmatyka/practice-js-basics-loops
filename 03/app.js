94394488a91697398b47dc6b9a71d5ac2bb1cd66
Tomasz Cividino <tomaszcividino@gmail.com>
2022-01-18 17:50:29 +0100

    03 basics

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
-const x = 10;
+const x = 5;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber != x) {
+    randomNumber = Math.floor(Math.random() * 10)
+    iteration++;
+}
+
+console.log(iteration)
+
