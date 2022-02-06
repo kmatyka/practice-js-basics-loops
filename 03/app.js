fbf804a693c9b67ad1cba513fcff48957ff6b6e1
Anna <krolikowska.ann@gmail.com>
2022-02-06 21:45:08 +0100

    03 added 'while' loop

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 15);
+    iteration++;
+    console.log(randomNumber);
+}
+
+console.log('Losowanie odbyło się '+ iteration +' razy, zanim wylosowano liczbę 10 ');
