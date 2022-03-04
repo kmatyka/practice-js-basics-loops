cbe91d1c11a11e4095591e610856f334dda2baf3
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-03-04 12:53:44 +0100

    Finish 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+
+    console.log(randomNumber, iteration);
+}
+
+console.log(iteration, 'Tyle iteracji potrzeba do wylosowania', x);
