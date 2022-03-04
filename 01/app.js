902e7a3a0fe6e7a8a82de1e0b9c141f7fc03a63f
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-03-04 12:33:03 +0100

    Finish 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,13 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if(randomNumber > 5) {
+    // zrób to jeśli większa od 5
+
+    for(let i=5; i<=randomNumber; i++) {
+        console.log(i);
+    }
+} else {
+    // zrób to jeśli nie
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
