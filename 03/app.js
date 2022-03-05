60230516d1462e82a32e851c876b85b233bf0c30
Piotr <piotr.wojcikievvicz@gmail.com>
2022-03-05 17:48:31 +0100

    completed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber!==x){
+    iteration+=1;
+    randomNumber=Math.round(Math.random()*20);
+   // console.log('Wylosowano liczbę: '+randomNumber);
+}
+
+console.log('Liczbę '+x+' wylosowano za '+iteration+' razem');
