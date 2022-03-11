d9711c132f4d8c504dc3737e3ff6c56c264e9db6
Lukasz Konatowski <konat1993@gmail.com>
2022-03-11 20:22:58 +0100

    task #1

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,12 @@
-const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+const randomNumber = Math.round(Math.random() * 20)
+
+if (randomNumber > 5) {
+    console.log(randomNumber)
+    let numbers = []
+    for (let i = 5; i <= randomNumber; i++) {
+        numbers.push(i)
+    }
+    console.log(numbers)
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
