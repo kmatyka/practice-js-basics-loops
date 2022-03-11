fc967b7e35368ace0ee48a767a31ef8b24b0a78c
Lukasz Konatowski <konat1993@gmail.com>
2022-03-11 20:46:59 +0100

    task #3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
-const x = 10;
-let iteration = 0;
-let randomNumber = -1;
+const x = 2
+let iteration = 0
+let randomNumber = -1
+
+while (x !== randomNumber) {
+    const findX = Math.round(Math.random() * x)
+    console.log({findX})
+    findX === x ? randomNumber = findX : console.log('jedziemy dalej')
+    iteration++
+}
+console.log({ iteration })
+console.log({ randomNumber })
