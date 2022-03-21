56004dc2170d30b3a16230cdc8ff30ddcee259bd
Filip <43039909+Urfisior@users.noreply.github.com>
2022-03-21 22:54:39 +0000

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,16 @@
 const x = 10;
 let iteration = 0;
-let randomNumber = -1;
+let randomNumber;
+
+while (randomNumber !== x) {
+    iteration += 1;
+    randomNumber = Math.floor(Math.random() * 20);
+    console.log(`randomNumber is ${randomNumber}`);
+};
+
+if (randomNumber === x) {
+    console.log(`While loop trying ${iteration} times to match your ${x} num`);
+};
+
+
+
