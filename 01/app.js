b3ec31c20f3f7029506054e98337530dde593e63
Filip <43039909+Urfisior@users.noreply.github.com>
2022-03-21 20:31:59 +0000

    task one done

--- 01/app.js
+++ 01/app.js
@@ -8,25 +8,4 @@ if (randomNumber > 5) {
     }
 } else {
     console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Twoim zadaniem jest napisać program, który będzie sprawdzał, czy wylosowana liczba jest większa od 5:
-//     -Jeśli tak, to należy wykorzystać pętle `for`, aby wyświetlić w konsoli wszystkie liczby od 5 do wylosowanej wartości.Jeśli więc program wylosuje liczbę 10, to w konsoli powinniśmy wyświetlić liczby: 5, 6, 7, 8, 9, 10. -
-//         Jeśli nie, to w konsoli należy wyświetlić informację: "Wylosowana liczba jest zbyt mała, aby użyć pętli".
-
-//     Zwróć uwagę na konstrukcję dwóch powyższych zdań: "Jeśli prawda, to zrób to; jeśli fałsz, to zrób coś innego".Oba jednoznacznie wskazują na to, że oprócz samej pętli `for`, należy w tym zadaniu wykorzystać również instrukcję warunkową `if`.
+};
