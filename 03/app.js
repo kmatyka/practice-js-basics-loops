aa97c5e04ba4ee2f7297d2144aed896320d2f941
Martyna Giee <martyna.giee@gmail.com>
2022-03-21 17:16:07 +0100

    done tasks

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * x);
+    iteration++
+}
+
+console.log(`Wykonano ${iteration} iteracji aby wylosować liczbę ${x}`)
