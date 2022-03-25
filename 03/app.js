bdaa3092fd97fd7ff7c8c13d60561ee0f8fea1cb
Andrzej <andis_as@wp.pl>
2022-03-25 22:42:37 +0100

    03 - done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,26 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++
+    console.log('iteretion: ' + iteration + ', random is: ' + randomNumber)
+    if(randomNumber === x) console.log('Aff, finally')
+}
+console.log('number of iteration is: ', iteration)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
