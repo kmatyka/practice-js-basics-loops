068dec402841bbe1a15d03f00c4491fd431157c5
justyna <strawberka17@gmail.com>
2022-03-28 12:24:39 +0100

    finished task 2 loops

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,24 @@
 const x = 5;
 let result = 0;
+console.log("hey");
+// for
+
+for(let i=1; i<=x ;i++){
+    result +=i
+   
+}console.log(result)
+
+
+// while
+let i=1
+while(i<=x){
+      result+=i;
+      
+
+      i++
+   
+  
+}
+
+ console.log(result)
+
