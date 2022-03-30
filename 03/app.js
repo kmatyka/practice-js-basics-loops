dc0654c4d09743e29a621db94639352964ab6a6e
Marta Stelmach <marta.ms1961@gmail.com>
2022-03-30 19:49:16 +0200

    Finish task-3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+console.log("Losujemy liczbę z przedziału 0-30. Za którym razem uda się wylosować liczbę " + x + "?");
+
+while (randomNumber != x) {
+    randomNumber = Math.round(Math.random() * 30);
+    iteration++;
+    console.log(randomNumber);
+};
+
+console.log("Udało się wylosować liczbę za " + iteration + " razem.");
