520beb7a23058ec134c97aa24af4533d0d21d45e
Camila Montes Celinski <camicelinski@gmail.com>
2022-03-30 16:46:28 +0200

    Complete task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,18 @@
 const x = 5;
 let result = 0;
+
+for(i=1; i<=x; i++) {
+    result = result + i;    
+}
+
+console.log(result)
+
+result = 0;
+let a = 1;
+
+while(a <= x) {
+    result = result + a;
+    a++;
+}
+
+console.log(result)
