d87559e1ccd37c2bcbcb822e5f190da65d481f33
Mateusz Suplewski <matx3582@gmail.com>
2022-03-30 12:05:51 +0200

    Basics-loops Done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,22 @@
 const x = 5;
 let result = 0;
+
+
+for (let i = 1; i <= x; i++) {
+
+    console.log('result = ' + result + ' + ' + i);
+    result += i;
+
+}
+
+console.log('Wynik końcowy to : ' + result);
+
+result = 0;
+let counter = 1;
+
+while (counter <= x) {
+    console.log('result = ' + result + ' + ' + counter);
+    result += counter;
+    counter++;
+}
+console.log('Wynik końcowy to : ' + result);
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+
+while (x != randomNumber) {
+    randomNumber = Math.round((Math.random() * x));
+    iteration++;
+}
+
+console.log("Do wylosowania liczby : " + x + " Potrzeba było " + iteration + " prób");
