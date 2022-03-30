9ff7d2a098b1457f4265262a5939cbe32dc74783
Marta Stelmach <marta.ms1961@gmail.com>
2022-03-30 19:36:46 +0200

    Finish task-2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+for (i = 0; i < x; i++) {
+    result = result + i + 1;
+    console.log(result);
+};
+
+let j = 0;
+let result_2 = 0;
+
+while (j < x) {
+    result_2 = result_2 + j + 1;
+    j++;
+    console.log(result_2);
+};
