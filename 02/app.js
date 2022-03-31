34dda905255f86d8996a8071a395b56a4f23ba87
Dawid Drag <dede7d@wp.pl>
2022-03-31 20:27:09 +0200

    added js code

--- 02/app.js
+++ 02/app.js
@@ -5,4 +5,16 @@ for (i = result + 1; i <= 5; i++) {
     result = result + i
     console.log(result);
 }
-console.log('Ostateczny wynik: ' + result)
+console.log('Ostateczny wynik pętli for: ' + result)
+
+
+let secondResult = 0;
+let y = 0;
+
+while (secondResult < x * 3) {
+    y = y + 1
+    secondResult = secondResult + y;
+    console.log(secondResult);
+}
+
+console.log('Ostateczny wynik pętli while ' + secondResult);
