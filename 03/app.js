209e0a4babd2460442fa47e99a7dd4ea8330d2f3
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-02 22:45:25 +0200

    zad 3 while

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !==x) {
+    randomNumber = Math.round(Math.random() *20);
+    console.log(randomNumber); //dla mojej informacji, można usunąć
+    iteration++;
+}
+console.log('Liczba została wylosowana za ' +iteration +' razem.');
