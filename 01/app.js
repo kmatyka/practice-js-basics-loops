6dc5fabcaecf9e430a49f0d7b0ea6329f9c78fba
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-02 22:20:04 +0200

    for and if done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+const value = 5;
+if(randomNumber > value) {
+    for(let i=value; i<=randomNumber; i++) {
+        console.log(i);
+    }
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
