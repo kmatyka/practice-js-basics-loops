83f4da5d5cd707fa7ce42160fb275cdb564f66f8
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-02 22:34:29 +0200

    zad2 while version

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,13 @@
 const x = 5;
 let result = 0;
-for(let i=1; i<=x; i++) {
-    result=result+i;
+// for(let i=1; i<=x; i++) {
+//     result=result+i;
+//     console.log(result);
+// }
+
+let cycle = 1;
+while(cycle <= x) {
+    result=result+cycle;
     console.log(result);
+    cycle++;
 }
