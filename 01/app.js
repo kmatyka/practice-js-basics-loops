b6649b76fc0a61cb8c17d3926a12fb37a14281b6
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2022-04-03 20:39:14 +0200

    tasks completed

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log(randomNumber)
+
+if (randomNumber > 5) {
+    for (i = 5; i <= randomNumber; i++)
+        console.log(i)
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
+
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+    result = result + i
+    console.log(result)
+}
+result = 0
+let j = 0
+while (j <= x) {
+    result = result + j
+    console.log(result)
+    j++
+}
