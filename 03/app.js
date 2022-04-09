04e7a2d837675a73a1ee334d4c4f7f6021680e61
Joanna Markiewicz <jmarkiewicz1@op.pl>
2022-04-09 19:33:15 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
-const x = 10;
+const x = 5;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 10);
+    iteration = iteration + 1;
+    console.log("losowa liczba:", randomNumber)
+
+}
+console.log("ilość interacji", iteration)
