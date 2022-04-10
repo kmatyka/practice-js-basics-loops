d0e47c2405fee93dada042f0d13241beedcc6a43
Mariana Kruk <krukmaryana@gmail.com>
2022-04-10 23:23:52 +0200

    second task

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,21 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+    result += i;
+    console.log(result);
+}
+console.log('ostateczna suma wszytkich liczb =', result);
+
+
+result = 0;
+
+let j = 1;
+while (j <= x) {
+    result += j;
+    console.log(result);
+    j++;
+
+
+}
+console.log('ostateczna suma wszytkich liczb obliczona za pomoca petli while=', result)
