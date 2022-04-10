80acd095417f01725b8a66087391a4903dbc53b5
Mariana Kruk <krukmaryana@gmail.com>
2022-04-10 22:44:47 +0200

    first task

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,16 @@
+//is randomNumber>5?
+//if yes then console.log all numbers from 5 till randomNumber
+//if no then console.log("Wylosowana liczba jest zbyt mała, aby użyć pętl")
+
+
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log('Wylosowana liczba randomowa =', randomNumber);
+
+if (randomNumber > 5) {
+    for (let i = 5; i <= randomNumber; i++) {
+        console.log(i);
+    }
+} else {
+
+    console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+}
