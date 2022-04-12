6821c19f3401d92d0e95273c5d58dcc6617442a3
Beata <beata.ptak11@gmail.com>
2022-04-12 18:19:28 +0200

    Zadania-petle

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+// console.log('za pomocą for:')
+// for(var i=1; i<=x; i++) {
+//     result = result + i
+// }
+//     console.log(result)
+
+    console.log('za pomocą while:')
+    var i =1
+    while(i<=5) {
+        result = result + i;
+        i++;
+    }
+    console.log(result)
