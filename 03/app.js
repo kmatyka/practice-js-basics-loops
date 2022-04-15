4f832a6403a98129a0b254524901c94c4f45e7c4
Pawel Podlesny <pawelpodlesny1196@gmail.com>
2022-04-15 22:04:03 +0200

    exercise-3-done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,9 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !==x) {
+    iteration+=1;
+    randomNumber = Math.round(Math.random() * 20);
+}
+console.log('Liczbę '+x+' wylosowano za '+iteration+' razem')
