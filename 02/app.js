802e6af9ead6ec34955e9c91d45e0e3b8e422c92
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-21 14:18:07 +0200

    for and while

--- 02/app.js
+++ 02/app.js
@@ -1,13 +1,16 @@
+console.log('Pętla For')
 const x = 5;
-let result = 0;
-// for(let i=1; i<=x; i++) {
-//     result=result+i;
-//     console.log(result);
-// }
+let resultFor = 0;
+for(let i=1; i<=x; i++) {
+    resultFor=resultFor+i;
+    console.log(resultFor);
+}
+console.log('Pętla While')
 
+let resultWhile = 0
 let cycle = 1;
 while(cycle <= x) {
-    result=result+cycle;
-    console.log(result);
+    resultWhile=resultWhile+cycle;
+    console.log(resultWhile);
     cycle++;
 }
