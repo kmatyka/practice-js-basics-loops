abfd32eddf5347b6b5512aa5715966166256a251
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-02 13:16:22 +0200

    Done 02 task

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,19 @@
 const x = 5;
 let result = 0;
+let resultWhile = 0;
+
+// for
+
+for (let i = 0; i <= x; i++) {
+	result += i;
+    console.log(result);
+}
+
+//while
+
+let j = 1;
+while (j <= x) {
+	resultWhile += j;
+	j++;
+    console.log(resultWhile);
+}
