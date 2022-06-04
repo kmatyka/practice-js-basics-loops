fc426a3eae38867abd3fdc77d8b59e9da036709b
Kamil Swistak <kamil.swistak.it@gmail.pl>
2022-06-04 14:31:00 +0200

    Task 3 complete

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,11 @@
-const x = 5;
-let iteration = 4;
-// let randomNumber = -1;
+const x = 10;
+let iteration = 0;
+let randomNumber = -1;
 
-const randomNumber = Math.round(Math.random() * 10)
 
-while(randomNumber !== x) {
-iteration = iteration + 1
+while (randomNumber !== x) {
+      randomNumber = Math.round(Math.random() * 20)
+      iteration++
 }
 
 
