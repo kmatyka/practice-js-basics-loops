f1fcd2cbaab044bf45022b549877dc7a8761315c
Norbert sasak <norbert.sasak@gmail.com>
2022-06-20 21:59:15 +0200

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+
+// for (let i = 1; i <= x; i++) {
+//   result = result + i;
+//   console.log(result);
+// }
+
+let i = 1;
+
+while (i <= x) {
+  result += i;
+  console.log(result);
+  i++;
+}
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
-const x = 10;
-let iteration = 0;
-let randomNumber = -1;
+let iteration = 1;
+const x = 5;
+const randomNumber = Math.round(Math.random() * 20);
+
+while (x !== randomNumber) {
+  const randomNumber = Math.round(Math.random() * 20);
+  if (x === randomNumber) {
+    console.log(`Wylosowałeś liczbę ${x} za ${iteration} razem`);
+    break;
+  }
+  iteration++;
+}
