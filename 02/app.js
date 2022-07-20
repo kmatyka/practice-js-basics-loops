2043101dddd4c201ab45ac6625cb958e1bc05d31
Zuza Ganobis <zuzaganobis@gmail.com>
2022-07-20 12:26:47 +0200

    Completed tasks

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+for (i = 1; i <= x; i++) {
+  result = result + i;
+  console.log(result);
+}
+
+// let i = 1;
+
+// while (i <= x) {
+//   result += i;
+//   i++;
+//   console.log(result);
+// }
