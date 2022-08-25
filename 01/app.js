d810f1cbda562d387783ee7106d1e3e270f42e88
Tomek2112 <kaminski_tomasz@interia.pl>
2022-08-25 11:41:38 +0200

    done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+for(let i=5; i>=20; i++) {
+    if(randomNumber > 5) {
+        console.log(i);
+    } else {
+        console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+    }
+}
