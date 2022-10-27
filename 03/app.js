c07b81634e21f2bae7998077268533809ed85a7c
Anna <aniamatrackasadurek@gmail.com>
2022-10-27 11:04:22 +0200

    exercise 3 finished

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !==x) {
+
+    randomNumber = Math.round(Math.random() * 5);
+    iteration = iteration + 1;
+    console.log(randomNumber);
+}
+
+alert('Gratulacje! Udało Ci się za' + iteration + 'razem');
