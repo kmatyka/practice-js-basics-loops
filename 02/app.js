6f225f0f2e8f7afdb9c59cbaee38a6a35fd26cc0
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-18 15:25:25 +0100

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
 const x = 5;
 let result = 0;
+let result2 = 0;
+
+for (let i = 0; i <= x; i++) {
+	result += i;
+	console.log(result);
+}
+
+console.log('--------');
+
+let j = 0;
+while (j <= 5) {
+	result2 += j;
+	console.log(result2);
+	j++;
+}
