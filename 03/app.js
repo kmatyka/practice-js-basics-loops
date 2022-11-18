ae4877f039f588c53c7d1f3f2afa509147bb0dfc
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2022-11-18 15:39:37 +0100

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
-let randomNumber = -1;
+let randomNumber;
+
+while (x !== randomNumber) {
+	randomNumber = Math.round(Math.random() * 20);
+	iteration++;
+}
+
+console.log(
+	`Aby wylosować przechowywaną liczbę, pętla wykonała się ${iteration} razy.`
+);
