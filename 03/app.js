367eeb430057107e5bc088e66db119752590c989
Michal Lata <maxpoz3212@gmail.com>
2022-12-20 14:33:02 +0100

    Add loop counter script

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * x);
+
+    iteration++
+}
+
+console.log(`Pętla musiała być wykonana ${iteration} razy`);
