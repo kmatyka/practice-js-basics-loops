67f49aa32c29d21523130588efd1dcdf4a2905b9
Michal Lata <maxpoz3212@gmail.com>
2022-12-20 13:15:33 +0100

    Add for and while loops program

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
 const x = 5;
 let result = 0;
+
+// For loop
+for (let i = 1; i <= x; i++) {
+    result += i;
+}
+console.log('Result "for" loop: ' + result);
+
+// While loop
+result = 0;
+let j = 1;
+while (j <= x) {
+    result += j;
+    j++;
+}
+console.log('Result "while" loop: ' + result);
