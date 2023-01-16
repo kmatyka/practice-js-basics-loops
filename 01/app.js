ee27ce63c38e5e268509e1b7fde23a4c47d2ad94
Lukasz Bednarski <luk.bdski@gmail.com>
2023-01-16 08:30:09 +0100

    add solved task

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log('wylosowana liczba:' + randomNumber);
+
+if(randomNumber >= 5){
+    for(let i = randomNumber;i<=20;i++){
+        console.log(i)
+    }
+}
+else if(randomNumber < 5) {
+    console.log('Wylosowana liczba jest zbyt mala aby ja wyswietlic');
+}
