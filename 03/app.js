73944c9945f37f2221351021c7c9bb92272caa1d
Lukasz Bednarski <luk.bdski@gmail.com>
2023-01-16 09:11:12 +0100

    add while loop and if condition

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,27 @@
+/*
+W zadaniu #01 miałeś przykład, w jaki sposób wylosować liczbę z przedziału 
+od 0 do 20. Teraz Twoim zadaniem będzie wylosowanie przy pomocy pętli while 
+liczby przechowywanej w zmiennej x. Wartość dla zmiennej x może być dowolna 
+z losowanego przedziału, czyli np. const x = 5.
+W zmiennej iteration masz zbierać informacje o tym, ile razy musiała być 
+wykonana pętla while, aby móc wylosować wartość przechowywaną w zmiennej x. 
+Wartość zmiennej iteration wyświetl w konsoli po zakończeniu wykonywania 
+pętli. Do przechowywania wylosowanej liczby możesz wykorzystać zmienną 
+randomNumber.
+Zanim zaczniesz rozwiązywać to zadanie, zastanów się, w którym miejscu 
+będziesz losować liczbę oraz jaki powinien być warunek dla pętli while.
+*/
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+    console.log(iteration)
+
+    if(randomNumber === x) {
+        console.log('liczba wykonanych iteracji:' + iteration);
+        console.log('liczba random:' + randomNumber)
+    }
+}
