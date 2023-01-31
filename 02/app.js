2e9ea72604d29b3e9c21605808bafc4c464094b0
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-01-31 17:23:11 +0100

    zadania kontrolne pętle

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,36 @@
 const x = 5;
 let result = 0;
+
+//for(i=0; i<=x; i++){
+
+ // result = result + i
+    
+  //  console.log(result)
+
+//}
+
+
+while(result <= x){
+    
+  console.log(result)
+  result++;
+  sum = result;
+  alert(result + sum)
+    
+}
+
+
+
+  
+  
+  
+ 
+
+  
+
+
+
+  
+  
+
+
