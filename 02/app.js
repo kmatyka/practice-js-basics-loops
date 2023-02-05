0d8e2f4f7ad3c4af0b460307d948009e9a4e071f
anetasobanska <sannettka@gmail.com>
2023-02-05 15:04:06 +0100

    Added while loop to the task #02

--- 02/app.js
+++ 02/app.js
@@ -2,6 +2,16 @@ const x = 5;
 let result = 0;
 
 for(i=result; i <= x; i++) {
+    console.log("for");
     console.log(result, i);
     console.log(result += i);
+}
+
+let j = 0;
+result = 0;
+while(j <= x) {
+    console.log("while");
+    console.log(result, j);
+    console.log(result += j);
+    j++;
 }
