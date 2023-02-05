cf2c3d11928636571ba055698235f6529dfae1a7
anetasobanska <sannettka@gmail.com>
2023-02-05 14:00:31 +0100

    Finish task #01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if(randomNumber >= 5) {
+    for (let num = randomNumber; num <= 20; num++) {
+        console.log(num++)        
+    }
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
