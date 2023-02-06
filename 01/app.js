4af6f700af48d25a6c18e216b82ec5fb4f1c76e2
anetasobanska <sannettka@gmail.com>
2023-02-06 14:58:05 +0100

    Correction of #01

--- 01/app.js
+++ 01/app.js
@@ -2,8 +2,8 @@ const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
 
 if(randomNumber >= 5) {
-    for (let num = randomNumber; num <= 20; num++) {
-        console.log(num++)        
+    for (let i=5 ; i <= randomNumber; i++) {
+        console.log(i)        
     }
 } else {
     console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
