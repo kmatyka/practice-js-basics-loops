497e0d84e1a6f65710b8421163a5952da39d0a9d
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-02-28 23:34:35 +0100

    finish ex 1-3

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,25 @@
 const x = 5;
 let result = 0;
+
+for(let i = 1; i <= x; i++) {
+    if(i===x) {
+        result=result+i;
+        console.log(result);
+    } else {
+        result=result+i;
+    }
+}
+
+result = 0;
+let j=1;
+
+while (j<=x) {
+    if(j===x) {
+        result=result+j;
+        console.log(result);
+        j++;
+    } else {
+        result=result+j ;
+        j++;
+    }
+}
