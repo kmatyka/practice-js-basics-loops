364aba44764cb0200c13331d2e8b5a24a463b5d8
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-13 20:58:26 +0100

    Added task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x !== randomNumber) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+}
+
+console.log('Potrzebna liczba iteracji do wylosowania x: ' + iteration);
