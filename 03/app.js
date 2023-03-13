d6b57dc768966085ed4fcfbea564e8d1dcf76d10
Piotr Mazurek <mazurekp93@gmail.com>
2023-03-13 21:57:46 +0100

    Finish task-03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
-let randomNumber = -1;
+let randomNumber;
+
+while (randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 10);
+    iteration++;
+}
+console.log(iteration)
+
