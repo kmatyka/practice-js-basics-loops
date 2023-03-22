1d96f6d12b239df4040455d9c0c358d0e69a08f5
Ireneusz Cislo <irekc86@o2.pl>
2023-03-22 06:36:17 +0100

    task 02 finished

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
 const x = 5;
 let result = 0;
+
+for(let i=1; i<=5; i++) {
+    result += i;
+    console.log(result);
+}
+
+result = 0;
+
+let j = 1;
+
+while(j<=5) {
+    result += j;
+    j++;
+    console.log(result);
+}
