c6c5f929a594d43c008449a6d9fb7a50a6295db3
Andrzej Przybylski <andrzej.prokocim@gmail.com>
2023-03-23 21:31:30 +0100

    Task 02 done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+  result = result + i;
+  console.log(i + '. ' + 'result = ' + result);
+}
+
+result = 0;
+
+let j = 1;
+while(j <= x){
+    result = result + j;
+    console.log(j + '. ' + 'result = ' + result);
+    j++;
+}
