35c13328e0929e30bbb752608b57c4c430171795
Ryuuken <wachnickiandrzej@gmail.com>
2023-03-23 18:51:26 +0100

    Loop exercise 1 done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,13 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log(`Wylosowana liczba: ${randomNumber}`);
+
+if(randomNumber > 5) {
+    for(let i = 5; i <= randomNumber; i++) {
+        console.log(i);
+    }
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+
+}
+
+
