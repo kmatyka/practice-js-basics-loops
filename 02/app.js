83aa88e688ee00015f5d633a80d97a4082d3179a
MS <maria.siedlik@gmail.com>
2023-03-26 20:01:04 +0200

    rozwiązania

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+// for(let i=1; i<=x ; i++){
+//    console.log("result = "+result+" + "+i);
+//    result = result + i;
+   
+// }
+
+let a=1;
+while(a<=x){
+   console.log("result = "+result+" + "+a);
+   result = result + a;
+   a++;
+}
+
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
-const x = 10;
+const x =5;
 let iteration = 0;
 let randomNumber = -1;
+
+// console.log(x);
+
+while(randomNumber!==x){
+   randomNumber = Math.floor(Math.random()*10);
+   iteration=iteration+1;
+   console.log(iteration);
+}
