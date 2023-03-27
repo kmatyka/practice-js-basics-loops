4c79d10111038f97fde1f033e7c182d29cba2e9c
Jakub Zalewski <kubaz-1992@wp.pl>
2023-03-27 11:13:18 +0200

    task 3 done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,14 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+
+let i=1;
+while(randomNumber !== x){
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+    console.log(randomNumber, iteration);
+}
+
+console.log(iteration, "Tyle losowań wykonano, żeby wylosować", x)
