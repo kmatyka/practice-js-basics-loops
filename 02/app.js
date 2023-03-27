42eefbc83d524ed31b1822596daa5d13f8eced8f
Jakub Zalewski <kubaz-1992@wp.pl>
2023-03-27 10:52:57 +0200

    finished 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,20 @@
 const x = 5;
 let result = 0;
+
+
+// for (let i = 1; i <= x; i++) {
+//       result = result + i;
+//       console.log("i=" + i, result)
+// }
+
+// console.log(result);
+
+
+let j=1;
+while (j<=5){
+      result = result + j;
+     
+      j++;
+}
+
+ console.log(result, "while")
