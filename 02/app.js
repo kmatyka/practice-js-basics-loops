7adcd56276bb095281097a64ca2d53c4b8029ec0
Maria Kardas <mariah.grze@gmail.com>
2023-04-05 00:10:38 +0200

    Finished task 02

--- 02/app.js
+++ 02/app.js
@@ -1,9 +1,20 @@
 const x = 5;
 let result = 0;
 
-for(let i=1; i<=5; i++) {
+// for(let i=1; i<=5; i++) {
+//     result = result + i;
+//     console.log('i=', i, result)
+// }
+
+// console.log(result)
+
+
+let i = 1
+
+while(i<=5) {
     result = result + i;
-    console.log('i=', i, result)
+
+    i++
 }
 
-console.log(result)
+console.log('while', result)
