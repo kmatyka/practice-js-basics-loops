c13ad51cbe6d68f563b40d3d869f071450b58b2f
kozerka <kozerka@gmail.com>
2023-04-07 15:27:19 +0200

    Complete tas 02 for loop

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,8 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+	result += i;
+	console.log(`Suma kolejnych ${i} liczb wynosi ${result}`);
+}
+console.log(`Suma kolejnych liczb od 1 do x wynosi ${result}`);
