9f0cccc956d52d5e51f436c8d833dd3b8b10de3f
mike161pl <mike161pl@gmail.com>
2023-04-27 17:45:05 +0200

    task finished

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,13 @@
-const x = 5;
-let result = 0;
+const x = 5
+let result = 0
+for (let i = 1; i <= x; i++) {
+	console.log(`result= ${result} + ${i}`)
+	result = result + i
+}
+console.log('------')
+
+let j = 0
+let result2 = 0
+while (x > j) {
+	console.log(`result= ${(result2 = result2 + j++)} + ${j}`)
+}
