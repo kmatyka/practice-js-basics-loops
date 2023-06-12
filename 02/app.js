afd3e0b37b7c3f6e05f660ef7a639678e7b7a99f
Wojciech Flasinski <wojciech.flasinski@gmail.com>
2023-06-12 21:48:13 +0200

    js-loops-finished

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,23 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+  // console.log(`${result} + ${i}`) 
+  result = result + i;
+  
+}
+console.log("pętla for");
+console.log(result);
+
+console.log("pętla while");
+
+let result2 = 0;
+
+let j = 0;
+while (j <= x) {
+  result2= result2 + j;
+  j++;
+  
+}
+console.log(result2);
+
