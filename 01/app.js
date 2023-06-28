043a12f7b0922646197fa10579283205e78bb0c0
MarcinK <marcin.kulbicki@gmail.com>
2023-06-28 13:37:32 +0200

    finish ex

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if (randomNumber > 5) {
+  let index = 5;
+  while (index <= randomNumber) {
+    console.log(index);
+    index++;
+  }
+}
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (x !== randomNumber) {
+  iteration++;
+  randomNumber = Math.round(Math.random() * 20);
+}
+
+console.log(iteration);
+console.log(randomNumber);
