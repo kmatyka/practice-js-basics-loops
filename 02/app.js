0783d83114b72cbfdab6ec9a77f12fdef56da5cb
Marcin Soltysik <profittax@gmail.com>
2023-07-03 23:18:15 +0200

    Done 02, for, while

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+  result = result + i;
+  console.log('for:', result);
+}
+
+let result1 = 0;
+let j = 1;
+while (j <= x) {
+  result1 = result1 + j;
+  j++;
+  console.log('while:', result1);
+}
