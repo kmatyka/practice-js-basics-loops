622a92b38971c6a9f2b33df785b0187afaee9625
Natalia Gron <nanusza@gmail.com>
2023-07-13 22:11:12 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,22 @@
 const x = 5;
 let result = 0;
+
+// pętla for
+
+for(let i=0;i<=x;i++){
+    result+=i;
+}
+
+console.log(result);
+
+// pętla while
+
+result = 0;
+
+let i=0;
+while(i<=x){
+    result+=i;
+    i++;
+}
+
+console.log(result);
