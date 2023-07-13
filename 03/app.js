575287d88232617d039bdb1a56ba945ba7461d2e
Natalia Gron <nanusza@gmail.com>
2023-07-13 22:32:41 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber!==x){
+    iteration++;
+    randomNumber = Math.round(Math.random()*20);
+}
+
+console.log('Zgadłeś za: ' + iteration + ' razem.');
