4afea9315f690db33d4813c68247b0781051ad61
Marcin <xcinek1337@wp.pl>
2023-07-28 15:25:59 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
 const x = 10;
 let iteration = 0;
-let randomNumber = -1;
+let randomNumber;
+
+while (x !== randomNumber) {
+	randomNumber = Math.floor(Math.random() * x + 1);
+	iteration++;
+    console.log(randomNumber);
+}
+console.log('ilość prób wylosowania liczby: ',iteration);
