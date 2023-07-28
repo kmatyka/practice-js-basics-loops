51411ba11e25f9798e8b4d0c6510b648c60bd27b
Marcin <xcinek1337@wp.pl>
2023-07-28 14:53:08 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+	result += i;
+	console.log(i + '.result = ' + result);
+}
+console.log('%c----------------', 'color: green;');
+let a = 1;
+while (a <= 5) {
+	result += a;
+	console.log(a + '.result = ' + result);
+	a++;
+}
