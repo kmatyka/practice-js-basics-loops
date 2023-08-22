6afa650983f3839ecb89995c7d5e907b6ac72db1
Piotr <you@example.com>
2023-08-22 21:18:38 +0200

    add task

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
-const x = 5;
-let result = 0;
+const x = 5
+let resultFor = 0
+
+for (let i = 1; i <= x; i++) {
+	resultFor = resultFor + i
+}
+console.log(resultFor)
+
+// while
+
+let resultWhile = 0
+let i = 1
+while (i <= x) {
+	resultWhile = resultWhile + i
+	i++
+}
+console.log(resultWhile)
