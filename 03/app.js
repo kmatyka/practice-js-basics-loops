5426e988a6e10fa1ad2532bc6f5c8237d29ad2eb
Marcin Chechelski <marcin.chechelski@op.pl>
2023-08-22 22:25:03 +0200

    tas 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,14 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+while (true) {
+    iteration++;
+    randomNumber = Math.ceil(Math.random() * x); // Losuje liczbę od 1 do x
+
+    if (randomNumber === x) {
+        break; // Przerwij pętlę, gdy wylosowana liczba jest równa x
+    }
+}
+
+// Wyświetlenie wyników
+console.log(`Wylosowano liczbę ${randomNumber} po ${iteration} iteracjach.`);
