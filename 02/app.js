9d7d1e2156439e029e37bdfddc0379b62f525319
Marcin Chechelski <marcin.chechelski@op.pl>
2023-08-23 22:24:01 +0200

    add while

--- 02/app.js
+++ 02/app.js
@@ -4,4 +4,11 @@ for (let i = 1; i <= x; i++) {
     result = result + i;
 }
 
-console.log("Wynik:", result);
+let result2 = 0;
+let j = 1;
+while (j <= x) {
+    result2 = result2 + j;
+    j++;
+}
+
+console.log("Wynik:", result, result2);
