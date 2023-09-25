251ae8b526c4b840f58760bf1b6a4538e6830047
Pawel Kielpinski <pawelkielpinski@gmail.com>
2023-09-25 14:37:10 +0200

    Loop exercises solution

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,14 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log(`Wylosowana liczba to: ${randomNumber}`);
+
+if (randomNumber > 5) {
+  let numberRange = [];
+
+  for (let i = 5; i <= randomNumber; i++) {
+    numberRange.push(i);
+  }
+
+  console.log(`Wylosowane liczby to: ${numberRange.join(", ")}`);
+} else {
+  console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+}
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,24 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i <= x; i++) {
+  console.log(`${i}. result = ${result} + ${i}`);
+
+  result += i;
+}
+
+console.log("Sum =", result);
+
+console.log("-------------------");
+
+let result2 = 0;
+let counter = 1;
+
+while (counter <= x) {
+  console.log(`${counter}. result2 = ${result2} + ${counter}`);
+
+  result2 += counter;
+  counter++;
+}
+
+console.log("Sum =", result2);
