edef1045c7cf6bb489f1713504a6d02bf19cb677
Michal Gawlik <gawlik.zator@o2.pl>
2023-09-26 13:03:28 +0200

    practice-js-basics-loops solved

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
-const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+const randomNumber = Math.round(Math.random() * 20)
+
+if (randomNumber > 5) {
+	for (let i = randomNumber; i <= 20; i++) {
+		console.log(i)
+	}
+} else {
+	console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
+
+console.log(randomNumber)
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,18 @@
-const x = 5;
-let result = 0;
+const x = 5
+let result = 0
+let result2 = 0
+
+for (let i = 1; i <= x; i++) {
+	result = result + (i - 1)
+	// console.log(i + '. ' + 'result =' + ' ' + result + '+' + i)
+	console.log(`${i}. result = ${result} + ${i}`)
+}
+
+console.log()
+
+j = 1
+while (j <= x) {
+	result2 = result2 + (j - 1)
+	console.log(`${j}. result = ${result2} + ${j}`)
+	j++
+}
