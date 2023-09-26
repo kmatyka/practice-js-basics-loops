6c0ada55931b975bebeac16582bb9f3bf126fb37
Michal Gawlik <gawlik.zator@o2.pl>
2023-09-26 13:02:28 +0200

    practice-js-basics-loops solved

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
-const x = 10;
-let iteration = 0;
-let randomNumber = -1;
+const x = 10
+let iteration = 0
+let randomNumber = 0
+
+while (x !== randomNumber) {
+	randomNumber = Math.round(Math.random() * 10)
+	iteration = iteration + 1
+}
+
+console.log(x)
+console.log(randomNumber)
+console.log(iteration)
