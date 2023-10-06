38b22bed1b734196ab559146b9ba15bdd40800cd
Aleksy Zydlewski <limeakiwano33@gmail.com>
2023-10-06 17:10:53 +0200

    v1

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+ for (i =result; i<=x; i++){
+     result = result+ i;
+     console.log (result);  
+ }
+
+j=result;
+
+while (j<x){
+    j++;
+    result= result +j;
+    console.log(result);
+}
