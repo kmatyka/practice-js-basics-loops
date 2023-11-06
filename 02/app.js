2e8d06bb527b0d35e5b00b8d6114b19e6349166d
Kasia <k.kasia912@gmail.com>
2023-11-06 16:14:07 +0100

    add for loop method taks 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 const x = 5;
 let result = 0;
+
+// FOR LOOP
+
+const addNumbers = (x) => {
+  for (let i = 0; i <= x; i++) {
+    result = result + i;
+  }
+  console.log(result);
+};
+addNumbers(x);
+
+// WHILE LOOP
