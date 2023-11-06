047c51b27e79a8da3a866d3d8a47c4607897fa70
Kasia <k.kasia912@gmail.com>
2023-11-06 15:48:55 +0100

    complete task 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,13 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+const checkNumber = (number) => {
+  if (number >= 5) {
+    for (let i = 4; i < number; i++) {
+      console.log(i + 1);
+    }
+  } else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+  }
+};
+checkNumber(randomNumber);
