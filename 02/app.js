1fcf1c70f873d2243a9db4fd9dc47c03c33a8a39
Krzysztof Swół <krzysztof.swol@salesmanago.com>
2023-11-14 22:52:07 +0100

    feat: task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,23 @@
 const x = 5;
 let result = 0;
+
+function sum() {
+  for(let i = 1; i <= x; i++) {
+    console.log(`${i}. result = ${result} + ${i}`);
+    result += i;
+    
+  }
+}
+
+sum();
+
+
+const y = 5;
+let resultTwo = 0;
+let count = 1
+
+while(count <= x) {
+  console.log(`${count}. result = ${resultTwo} + ${count}`);
+  resultTwo += count;
+  count++;
+}
