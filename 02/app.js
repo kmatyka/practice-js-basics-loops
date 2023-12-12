24fc632e54d3cc6afc8171e2e5900b83b5bffad9
Dawidtk <dawidtk@gmail.com>
2023-12-12 08:53:34 +0100

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,28 @@
-const x = 5;
-let result = 0;
+/*  const x = 5;
+    let result = 0;
 
-for(let i=1; i<=x; i++) {
-    result = result + i;
+    for(let i=1; i<=x; i++) {
+        result = result + i;
+    }
+
+    console.log(result);
+
+    let i = 1;
+    while(i<=5) {
+        result = result + i;
+
+        i++;
+    }
+
+    console.log(result, 'loops while'); */
+
+const x = 10;
+let iteration = 0
+let randomNumber = -1;
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration++;
+    console.log(randomNumber, iteration)
 }
-
-console.log(result);
+console.log(x,'Finished for',iteration, 'times')
