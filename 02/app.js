208ecdcca5a83e01b44703596c82d7a7dee2df38
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-17 21:29:52 +0100

    Create for and while loop

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,21 @@
+// for 
 const x = 5;
 let result = 0;
+
+for(let i=0; i<=x; i++) {
+    result += i; 
+    console.log(result);
+}
+console.log("Sumba liczb od 1 do", x, "wynosi:", result);
+
+
+// while
+let resultWhileLoop = 0;
+let i = 0;
+
+while(i<=x) {
+    resultWhileLoop += i;
+    i++;
+    console.log(resultWhileLoop);
+}
+console.log("Sumba liczb od 1 do", x, "wynosi:", resultWhileLoop);
