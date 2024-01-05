056d0796aee222e6a4d28949dff183477daae8f8
Marcin Wabinski <wabinskim@gmail.com>
2024-01-05 10:27:38 +0100

    Task finished

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
 const x = 5;
 let result = 0;
+
+for (i = 0; i <= x; i++) {
+  result = result + i;
+  console.log(result);
+}
+console.log("Suma wynosi " + result);
+
+let result2 = 0;
+j = 0;
+while (j <= x) {
+  result2 = result2 + j;
+  j++;
+  console.log(result2);
+}
+console.log("Suma wynosi " + result2);
