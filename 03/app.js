9ee0ae657587885a9dbdbe526de347d4942b92d5
Marcin Wabinski <wabinskim@gmail.com>
2024-01-05 10:54:41 +0100

    Task finished

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,17 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+  randomNumber = Math.round(Math.random() * 20);
+  iteration++;
+  console.log(
+    "Iteracja nr " + iteration + " Wylosowana liczba to " + randomNumber
+  );
+}
+console.log(
+  "Pętla musiała zostać wykonana " +
+    iteration +
+    " razy, żeby znaleźć liczbę przechowywaną w zmiennej x, czyli " +
+    x
+);
