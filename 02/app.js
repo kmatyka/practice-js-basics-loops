3499a37b9a3767fba25fbb216b08614fc15a5cd6
Marcin Wabinski <wabinskim@gmail.com>
2024-01-09 10:50:28 +0100

    Update task

--- 02/app.js
+++ 02/app.js
@@ -1,14 +1,14 @@
 const x = 5;
 let result = 0;
 
-for (i = 0; i <= x; i++) {
+for (let i = 0; i <= x; i++) {
   result = result + i;
   console.log(result);
 }
 console.log("Suma wynosi " + result);
 
 let result2 = 0;
-j = 0;
+let j = 0;
 while (j <= x) {
   result2 = result2 + j;
   j++;
