55d8563d7f77cb07dc7d5dc67b49895a268bd44c
Rafal <rafk528@gmail.com>
2024-01-23 20:33:10 +0000

    Iteration numbers

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
-const x = 10;
-let iteration = 0;
-let randomNumber = -1;
+const x = 5
+let iteration = 0
+let randomNumber
+
+while (randomNumber !== x) {
+  randomNumber = Math.floor(Math.random() * 10) + 1
+  iteration++
+}
+
+console.log("Liczba iteracji: " + iteration)
