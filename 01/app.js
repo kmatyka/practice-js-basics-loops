bbf91c6d1963b4c8b152132224b30034fc60b506
Paweł Suder <pawel.suder94@gmail.com>
2024-02-07 09:13:12 +0100

    task 1 done

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log(randomNumber + 'to');
+
+if (randomNumber > 5) {
+    for (let i = 5; i <= randomNumber; i++) {
+        const result = i
+        console.log(result)
+    }
+} else {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli')
+}
