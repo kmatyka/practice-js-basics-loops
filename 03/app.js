8c0c851547418e2b58bb71ed1395f1d88d8618b5
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-11 21:31:46 +0100

    03 completed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,15 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+// let i;
+// i <= 20 && i >= 0;
+
+// while(randomNumber !== x) {
+//     i = Math.round(Math.random() * 20);
+//     iteration += iteration;
+    
+//     if(randomNumber === x) {
+//         console.log(randomNumber)
+//     }
+// }
