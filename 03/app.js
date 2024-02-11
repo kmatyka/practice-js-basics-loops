c6ad15608e55bd9bac12e2137c8aaf52ae9b8a04
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-11 21:31:52 +0100

    03 completed

--- 03/app.js
+++ 03/app.js
@@ -2,14 +2,13 @@ const x = 10;
 let iteration = 0;
 let randomNumber = -1;
 
-// let i;
-// i <= 20 && i >= 0;
-
-// while(randomNumber !== x) {
-//     i = Math.round(Math.random() * 20);
-//     iteration += iteration;
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration = 1;
+    iteration += iteration;
+    console.log(randomNumber)
     
-//     if(randomNumber === x) {
-//         console.log(randomNumber)
-//     }
-// }
+    if(randomNumber === x) {
+        console.log('Trafiłaś za ' + iteration + ' razem.')
+    }
+}
