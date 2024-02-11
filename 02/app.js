f36fd3168102a6389737cedbff6d95c4c8d75c71
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-11 11:16:49 +0100

    02 completed

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+for(i = 1; i <= x; i ++) {
+    result += i;
+    console.log(result);
+}
+
+
+let i = 1;
+
+while(i <= x) {
+    result += i;
+    console.log(result);
+    i ++;
+}
