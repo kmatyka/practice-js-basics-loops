0f0b5ac08970fed233c26412c48909899044a469
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-03-02 19:27:01 +0100

    add tasks

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,23 @@
 const x = 5;
 let result = 0;
+
+
+
+for (let i = 0; i < 5 + 1; i++) {
+
+    result += i;
+    console.log(result);
+}
+
+
+
+/* WHILE LOOP
+let i=0;
+while(i<=5) {
+result += i;
+  i++;
+
+  console.log(result);
+}
+
+*/
