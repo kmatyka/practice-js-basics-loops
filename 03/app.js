ab73ec937f0c19b7de344f0f1705087bd1a11406
MarcinMorgan <morgangzit@gmail.com>
2024-03-15 22:52:53 +0100

    feat: t3 done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
-const x = 10;
+const x = Math.round(Math.random() * 10);
 let iteration = 0;
 let randomNumber = -1;
+
+console.log('Poszukwiana liczba: '+ x)
+while(randomNumber!=x) {
+    iteration++;
+    randomNumber = Math.round(Math.random() * 10);
+    console.log('Wylosowana liczba: '+ randomNumber);
+    console.log('Iteracja: '+ iteration);
+}
+console.log('Koniec poszukiwań!');
