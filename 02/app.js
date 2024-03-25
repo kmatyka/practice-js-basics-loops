a2a311caef12d0d8f5860d1fc3acd3a671dea35d
Emilia Szafarska <emilia.anna.szafarska@gmail.com>
2024-03-25 19:11:04 +0100

    Add solution to task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,21 @@
 const x = 5;
 let result = 0;
+
+for(let i = 1; i <= x; i++) {
+  result += i;
+}
+
+console.log('Suma liczb od 1 do', x, 'to:', result)
+
+// Wersja z while
+
+// const x = 5;
+// let result = 0;
+// let i = 1
+
+// while(i <= x) {
+//   result += i;
+//   i++
+// }
+
+// console.log('Suma liczb od 1 do', x, 'to:', result)
