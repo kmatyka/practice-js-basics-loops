5f844fc2fba04fb7ee3a3148a18c159f09fbe021
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-04-02 13:05:07 +0200

    Fix task 2 while loop

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,13 @@
-const x = 5;
-let result = 0;
+const x=5;
+let result=0;
 
-for (let i = 1; i <= x; i++) {
+for (let i=1; i <= x; i++) {
   result += i;
   console.log(`result = ${result}`);
 }
 
+
+result=0;
 let j=1;
 while (j <= x) {
   result += j;
