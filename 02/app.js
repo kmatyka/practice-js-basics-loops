cb51daeb04a23d7b7452c378fa4435d3a3416c98
Grzegorz Stepien <grzesiek94stepien@gmail.com>
2024-04-07 21:02:34 +0200

    finish 02 and while

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
 const x = 5;
 let result = 0;
+
+// for(let i=1; i<=x; i++){
+//     result = result + i;
+// }
+
+// console.log(result)
+
+let i = 1;
+while(i<=5){
+    result = result + 1;
+
+    i++;
+}
+
+console.log(result, 'while');
