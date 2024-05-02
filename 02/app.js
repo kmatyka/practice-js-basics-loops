4449a2aa80bb02ef9fed2d804aa2d7a7305ae1f5
BartekB94 <wurir1994@gmail.com>
2024-05-02 19:07:41 +0200

    tasks complete

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+    result = result + i
+}
+console.log(result)
+
+// while loop
+let result2 = 0;
+
+while(result2 !== 15 ) {
+    result2++
+}
+console.log(result2)
