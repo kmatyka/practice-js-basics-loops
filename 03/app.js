dfda127d6379a59e0e66e9e44880ab9d8e40f368
PatrickStojek <patryk.stojek7@gmail.com>
2024-05-05 17:17:44 +0200

    exercise 3 completed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+///Rozwiązanie zadania
+while(randomNumber !== x) {
+    randomNumber = Math.floor(Math.random() * 21)
+    iteration++
+}
+
+console.log(iteration)
