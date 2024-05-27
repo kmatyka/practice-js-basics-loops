0a5d8c530872850b45be692f6122f28fd73d4335
Darek Mazur <dev@darekmazur.it>
2024-05-27 22:29:12 +0200

    Implement solution

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,18 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+
+const errorMsg = 'Wylosowana liczba jest zbyt mała, aby użyć pętli';
+
+const logNumbers = () => {
+	if (randomNumber >= 5) {
+		for (let i = 5; i <= randomNumber; i++) {
+			console.log(i)
+		}
+	} else throw new Error(errorMsg);
+}
+
+try {
+	logNumbers()
+} catch (error) {
+	console.error(error.message)
+}
+
