bef251808de4b7d99254937917972033f536c698
Kamil <kmatyka.15012004@gmail.com>
2024-05-31 17:01:34 +0200

    Zadanie 1

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,14 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+if(randomNumber>5){
+    let liczby = "";
+    for(i=5;i<=randomNumber;i++){
+        if(i+1>randomNumber){
+            liczby += i+".";
+        }else{
+            liczby += i+",";
+        }
+    }
+    console.log(liczby);
+}else{
+    console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+}
